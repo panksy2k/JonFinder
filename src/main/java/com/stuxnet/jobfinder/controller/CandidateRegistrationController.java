67b7d919160d0ed6a31b88e80c49342a2e2f24d6
Pankaj Pardasani <pankaj.d.p@gmail.com>
2020-06-11 09:06:48 +0100

    Revert "added retry onfailure options for container"
    This reverts commit c46edb9ed1235424bc83cdc09a6442f2a7157c16.

--- src/main/java/com/stuxnet/jobfinder/controller/CandidateRegistrationController.java
+++ src/main/java/com/stuxnet/jobfinder/controller/CandidateRegistrationController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.lang.invoke.MethodHandles;
-import java.util.Random;
 
 @Api(value = "REST Api for registering the candidate information")
 @RestController
@@ -27,8 +26,8 @@ public class CandidateRegistrationController {
     })
     @PostMapping(path = "/register", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<Integer> registerCandidate(@ApiParam(required = true, type = "CandidateRegistration.class") @RequestBody CandidateRegistration candidate) {
+    public ResponseEntity<Long> registerCandidate(@ApiParam(required = true, type = "CandidateRegistration.class") @RequestBody CandidateRegistration candidate) {
         log.info("Registering the candidate from {}", candidate.toString());
-        return ResponseEntity.ok(new Random().nextInt(100));
+        return ResponseEntity.ok(1L);
     }
 }
